d79d693ffd39106130491ff8dbc185a92d8bd82d
Yousef Mahmoud <mryoyo133@gmail.com>
2024-05-04 17:29:46 +0300

    Implementing JWT

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('subjects/', views.getSubjects, name='subjects-api'),
     path('subjects/<str:subject>', views.getTests, name='tests-api'),
     path('tests/<int:id>', views.getTest, name='GetTest-api')
